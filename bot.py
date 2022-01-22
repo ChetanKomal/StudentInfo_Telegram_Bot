aded9479c0fc6ff113ad30ec7bf4470e6c78d03b
Chetan Komal <76816799+ChetanKomal@users.noreply.github.com>
2022-01-22 20:31:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import openpyxl as xl
 import telebot
+import time
 from time import sleep
 my_secret = "YOUR_KEY"
 
@@ -11,12 +12,12 @@ bot = telebot.TeleBot(API_KEY)
 # def hello(message):
 #     bot.send_message(message.chat.id,message.text)
 def find(chetan):
- wb = xl.load_workbook("h1.xlsx")
+ wb = xl.load_workbook("h1copy.xlsx")
  ws = wb.active
  a = ["0","1","rollno: ","Student Name: ","Father Name: ","Gender: ","Branch: ","Group: ","specialization: ","Mobile no: ","Email: ","Status: "]
  tmp = []
  x=chetan
- for row in range(3,259):
+ for row in range(3,368):
      if(int(ws[f"B{row}"].value) == int(x)):
          for i in range(3,12):
              y=chr(64+i)
@@ -26,9 +27,16 @@ def find(chetan):
  
  return tmp           
 
+def write_to_file(message):
+  file = open("log.txt","a+")
+  data = ["time: ",time.ctime(),"command: ",message.text,"ID: ",message.from_user.id,"username: ",message.from_user.username,"firstname: ",message.from_user.first_name,"lastname: ",message.from_user.last_name]
+  file.write(str(data))
+  file.write("\n\n")
+  file.close()
 
 @bot.message_handler(commands=["help","start"])
 def helper(message):
+  write_to_file(message)
   info = ''' 
          This Bot gives information about students of B.E CSE Branch Batch 2019 CUHP. Just Pass a valid rollno.
          \n Bot Commands:\n{valid_university_id}(Eg. 191198xxxx) \n /help \n /start \n /moreinfo\n  \nDeveloped by: @CK
@@ -36,6 +44,7 @@ def helper(message):
   bot.send_message(message.chat.id,info)
 @bot.message_handler(commands=["moreinfo"])
 def moreinfo(message):
+ write_to_file(message)	
  ing = '''
        This bot provides the following information about students:\n
        Student Photo (if available)
@@ -57,7 +66,7 @@ def moreinfo(message):
  bot.send_message(message.chat.id,ing)
 @bot.message_handler(regexp="191198\d\d\d\d")
 def rep(message):
- 
+  write_to_file(message)
   if(find(message.text) != []):
    tmp = find(message.text)
    ff = "\n"+"\n" + "ID: " + message.text + "\n" + tmp[0] + "\n"+tmp[1]+"\n"+tmp[2]+"\n"+tmp[3]+"\n"+tmp[4]+"\n"+tmp[5]+"\n"+tmp[6]+"\n"+tmp[7]+"\n"+tmp[8]+"\n" +"\n" + "\n"+"Developed by @CK"
@@ -71,6 +80,7 @@ def rep(message):
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
+	write_to_file(message)
 	bot.send_message(message.chat.id,"Incorrect RollNumber")  
 
 print("Bot running")
